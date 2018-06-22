76a2b9006cccaab9b2a881d013ee5c35e17137b7
DiulinVitalii <vitalik.diulin@gmal.com>
2018-06-22 15:18:12 +0300

    was added main-home.html

--- app/js/init.js
+++ app/js/init.js
@@ -1,13 +1,25 @@
 ;(function($){
     //init ion range slider
-    $("#price-range").ionRangeSlider({
-        type: "double",
-        grid: false,
-        min: 0,
-        max: 1000,
-        from: 200,
-        to: 800,
-        prefix: "$",
-        hide_min_max: true
+    if ($('#price-range').length) {
+        $("#price-range").ionRangeSlider({
+            type: "double",
+            grid: false,
+            min: 0,
+            max: 1000,
+            from: 200,
+            to: 800,
+            prefix: "$",
+            hide_min_max: true
     });
+}
+    //init slick slider
+    if ($('.trending-products-carousel').length) {
+        $('.trending-products-carousel').slick({
+            dots: true,
+            nextArrow: false,
+            prevArrow: false,
+            autoplay: true,
+            autoplaySpeed: 3000
+        });
+    }
 })(jQuery);
